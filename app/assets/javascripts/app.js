5f1a87ee8aa0e137137bc8f38464fb7427d7ba24
Brad Wilcox <brad_wilcox@me.com>
2013-07-23 17:02:19 -0400

    setting AngularJS up to use the hamlc templates

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,3 +8,24 @@ app.config(function ($routeProvider) {
                 controller: 'StaticController',
                 templateUrl: '/app/assets/templates/context.js.coffee',
             })
+
+
+@photogur.config ($routeProvider, $locationProvider) ->
+
+  # Enable HTML5 history support. For browsers that support HTML5 history this 
+  # means you can use a url like this
+  #    http://localhost:3000/pictures/new
+  # Instead of this
+  #    http://localhost:3000/#/pictures/new
+  # 
+  # Here's a detailed explanation of how HTML5 history works: 
+  # http://diveintohtml5.info/history.html
+  $locationProvider.html5Mode(true)
+
+  $routeProvider
+  .when("/",                  templateUrl: "pictures/index")
+  .when('/pictures',          templateUrl: "pictures/index")
+  .when('/pictures/:id',      templateUrl: "pictures/show")
+  .when('/pictures/new',      templateUrl: "pictures/new")
+  .when('/pictures/:id/edit', templateUrl: "pictures/edit")
+  .otherwise(template: "Page not found.")
